93085650d273130eb8ce598d6a4dca27fa6eec9a
ArpKevin <arpaskevin11@gmail.com>
2025-03-07 08:47:55 +0100

    Add project files.

---
+++ RealEstate/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RealEstate
+{
+    internal class Program
+    {
+        static List<Ad> ads = new List<Ad>();
+        static void Main(string[] args)
+        {
+            ads = Ad.LoadFromCsv(@"..\..\..\src\realestates.csv");
+
+            Console.WriteLine($"1. Földszinti ingatlanok átlagos alapterülete: {Math.Round(ads.Where(a => a.Floors == 0).Average(a => a.Area), 2)} m2");
+
+
+
+            Console.ReadKey();
+        }
+
+        
+        //static double DistanceTo(double a1, double a2, double b1, double b2)
+        //{
+
+        //}
+    }
+}
